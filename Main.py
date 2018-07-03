c2c33b60218d6ae93aa38fd5caf5b2d36249ad7e
rcpsilva <rcpsilva@gmail.com>
2018-07-03 17:49:07 -0300

    Bug fixes

--- Main.py
+++ Main.py
@@ -4,20 +4,19 @@ Created on Jun 30, 2018
 @author: rcpsi
 '''
 from SilvaIncrementalFTS import SilvaIncrementalFTS as sFTS
+from pyFTS.benchmarks import benchmarks as bc 
 from pyFTS.data import TAIEX
-import numpy as np
+
 
 def main():
     # Fuzzy set type
 
-    #fts = sFTS(nsets = 7, do_plots = True)
-    order = 1
-    fts = sFTS(do_plots = False, order = 1)
+    #fts = sFTS(do_plots = False)
 
     data = TAIEX.get_data()
-    data = list(data[0:1500]) + list(data[0:1500]*3 - 5000) + list(data[0:1500])  
+
     
-    fts.train(data[0:(order+1)])
+    fts.train(data[0:2])
     fts.forecast(data[2:len(data)])
     
 
