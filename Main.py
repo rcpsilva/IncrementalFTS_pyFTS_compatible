33a802d18577660efccd05a241e47ee5a1ec6eb5
rcpsilva <rcpsilva@gmail.com>
2018-07-09 11:06:07 -0300

    Test for distribution restart

--- Main.py
+++ Main.py
@@ -4,22 +4,30 @@ Created on Jun 30, 2018
 @author: rcpsi
 '''
 from SilvaIncrementalFTS import SilvaIncrementalFTS as sFTS
-from pyFTS.benchmarks import benchmarks as bc 
+from SilvaIncDistributionRestartFTS import SilvaIncDistributionRestartFTS as rFTS
 from pyFTS.data import TAIEX
-
+from matplotlib import pyplot as mplt
+import numpy as np
 
 def main():
     # Fuzzy set type
 
-    #fts = sFTS(do_plots = False)
+    print('Testing  SilvaIncDistributionRestartFTS')
+    
+    fts = rFTS(do_plots = True)
 
     data = TAIEX.get_data()
-
+    #data = data - data[0]
+    data = list(data) + list(data*10 - np.mean(data)) + list(data)
     
+
     fts.train(data[0:2])
-    fts.forecast(data[2:len(data)])
+    forecasts = fts.forecast(data[2:len(data)])
     
-
+    mplt.plot(np.arange(2,len(data))+1,forecasts,'b')            
+    mplt.plot(np.arange(2,len(data)),data[2:len(data)],'r')
+    mplt.show()
+    
     fts.print_rules()
         
 
